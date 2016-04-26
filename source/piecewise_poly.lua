248d368f8a3e67593ce9550fc3615242e7d7b97b
KwirkyJ <kwirkyj.smith0@gmail.com>
2016-04-26 17:38:02 -0500

    Ported unit test to luaunit; added equality; started root-finding

--- source/piecewise_poly.lua
+++ source/piecewise_poly.lua
@@ -1,4 +1,8 @@
-local Piecewise = {}
+---Module for piecewise polynomial functions.
+
+local moretables = require 'lib.moretables'
+
+local piecewise = {}
 
 ---Add a new piece to the polynomial.
 local function addPiece(self, t1, coeffs)
@@ -64,23 +68,54 @@ end
 -- supports only polynomials of degree two or less.
 --TODO: define how to handle constants
 local function root(self, v)
-    return
+    v = v or 0
+    assert(type(v) == 'number', 'value must be number, was: '..type(v))
+    local roots, t_start, coeffs = {}
+--    for i=1, #self[1] do
+--        local t_start, coeffs = self[1][i][1], self[1][i][2]
+--        --assert(type(t_start) == 'number')
+--        --assert(type(coeffs) == 'table')
+--        --TODO
+--    end
+    if self[1][1] then
+        local coeffs = self[1][1][2]
+        if #coeffs > 0 then
+            local a, b, c = coeffs[1], coeffs[2], coeffs[3]
+            roots[#roots+1] = (-b - (b^2 - 4*a*c)^0.5) / 2*a
+            roots[#roots+1] = (-b + (b^2 - 4*a*c)^0.5) / 2*a
+        end
+    end
+    return roots
+end
+
+---Stand-in for '=='
+piecewise.areEqual = function(p1, p2)
+    if not (type(p1[1]) == 'table')
+    or not (type(p2[1]) == 'table')
+    or not p1.getStarts
+    or not p2.getStarts
+    or not moretables.alike(p1[1], p2[1])
+    then 
+        return false
+    end
+    return true
 end
 
 ---Create a new piecewise polynomial 'object'.
-Piecewise.Polynomial = function()
+piecewise.Polynomial = function()
     local pp = {{},
                 add         = addPiece,
                 clearBefore = clearBefore,
                 getStarts   = getStarts,
                 evaluate    = evaluate,
+                root        = root,
                }
     local mt = {__call = evaluate,
-                -- __eq = nil,
+                __eq   = piecewise.areEqual,
                }
     setmetatable(pp, mt)
     return pp
 end
 
-return Piecewise
+return piecewise
 
--- test/piecewise_poly.lua
+++ test/piecewise_poly.lua
@@ -1,54 +1,109 @@
--- spec and unit-test for the Piecewise Polynomial class/module
+-- spec and unit-test for the piecewise Polynomial class/module
 
-local Piecewise = require 'source.piecewise_poly'
+local piecewise = require 'source.piecewise_poly'
+local luaunit = require 'luaunit.luaunit'
 
 local pp, tmp
 
-pp = Piecewise.Polynomial()
-assert(pp(math.random(-100,100)) == nil, 'empty polynomial is nil')
+pp = piecewise.Polynomial()
+TestPolynomial = {}
+TestPolynomial.test_empty = function(self)
+    local p
+    for i=1, 100 do
+        p = piecewise.Polynomial()
+        assertNil(p(i-50), 'empty polynomial should be nil')
+    end
+end
+TestPolynomial.test_one_piece = function(self)
+    local p = piecewise.Polynomial()
+    p:add(0, {1, -2})
+    
+    assertEquals( p:getStarts(), {0}, 'confirm one piece which starts at zero')
+    assertNil(p:evaluate(-1), 'pre-start should be undefined')
+    assertEquals(p:evaluate(0), -2, 'f(0) -> 1*(0) + -2 -> -2')
+    assertEquals(p:evaluate(5),  3, 'f(5) -> 1*(5) + -2 ->  3')
+    
+    assertEquals(p(5), p:evaluate(5), 'metatable call as alias for evaluate')
+end
+TestPolynomial.test_more_pieces = function(self)
+    local p = piecewise.Polynomial()
+    p:add(3, {-0.5, 3, 0, 2})
+    p:add(0, {1, -2}) -- added before extant
+    p:add(2.2, {8})   -- inserted between extant
+    
+    assertEquals(p:getStarts(), {0,2.2,3}, 'three additions should self-order')
+    assertNil(p(-1), 'pre-start must be undefined')
+    assertEquals(p(1), -1, 'first piece domain; 1*1 - 2')
+    assertEquals(p(2.5), 8, 'inserted linear function')
+    assertAlmostEquals(p(5), (-0.5*5^3 + 3*5^2 + 2), 1e-12, 'cubic')
+end
+TestPolynomial.test_clearBefore = function(self)
+    local p = piecewise.Polynomial()
+    p:add(0, {1, -2}) -- for t≤0 return (1*t - 2)
+    p:add(2, {8})
+    p:add(3, {-0.5, 3, 0, 2}) -- -1/2*t^3 + 3*t^2 + 0*t + 2 
+    p:clearBefore(2.2)
+    
+    assertEquals(p:getStarts(), {2.2, 3}, 'clearBefore should trim')
+    assertEquals(p(2.99), 8)
+    assertEquals(p(3), -0.5*3^3 + 3*3^2 + 2)
+    assertNil(p(2.1999), 'new undefined region')
+end
+TestPolynomial.test_equality = function(self)
+    local p1, p2 = piecewise.Polynomial(), piecewise.Polynomial()
+    assertNotEquals(p1, nil)
+    assertNotEquals(p1, {})
+    assertEquals(p1, p2) --, 'unique empty instances')
+    
+    p1:add(3, {4, 0, -0.32, 0})
+    assertNotEquals(p1, p2)
+    
+    p2:add(3, {4, 0, -0.32, 0})
+    assertEquals(p1, p2)
+    assertEquals(piecewise.areEqual(p1, p2), true)
+    
+    p1:add(4, {3, 2})
+    p2:add(4, {2, 3})
+    assertNotEquals(p1, p2)
+    assertEquals(piecewise.areEqual(p1, p2), false)
+end
 
-pp:add(0, {1, -2}) -- for t≤0 return (1*t - 2)
-tmp = pp:getStarts()
-assert(#tmp == 1, 'one piece confirmed')
-assert(tmp[1] == 0)
 
-assert(pp(0) == -2)
-assert(pp(5) == 5-2)
-assert(pp(-2) == nil, 'pre-piece is undefined')
 
-pp:add(3, {-0.5, 3, 0, 2}) -- -1/2*t^3 + 3*t^2 + 0*t + 2 
-tmp = pp:getStarts()
-assert(#tmp == 2, 'confirm two pieces')
-assert(tmp[1] == 0)
-assert(tmp[2] == 3)
-assert(math.abs( pp(5) - (-0.5*125 + 3*25 + 2) ) < 1e-12, 
-       'second piece now at work') -- 14.5
-assert(pp(1) == 1-2, 'first piece still valid')
-assert(pp(-2) == nil, 'pre-piece is still undefined')
+TestRoot = {}
+TestRoot.setUp = function(self)
+    self.p = piecewise.Polynomial()
+end
+TestRoot.test_empty = function(self)
+    assertEquals(self.p:root(7), {}, 'empty polynomial has no root')
+end
+--TestRoot.test_constant = function(self)
+--end
+TestRoot.test_quadratic_tworoots = function(self)
+    self.p:add(3, {1, -3, 2}) 
+    --  x^2 - 3x + 2
+    --  x=(-b +/- sqrt(b^2 - 4ac)) / 2a 
+    --> (3 +/-1) / 2
+    assertEquals(self.p:root(0), {1, 2})
+    assertEquals(self.p:root(), self.p:root(0), 'default root of zero')
+end
+--TestRoot.test_quadratic_noroot = function(self)
+--end
+--TestRoot.test_quadratic_oneroot = function(self)
+--end
+--TestRoot.test_quadratic_tworoots_valueshift = function(self)
+--end
+--TestRoot.test_multiple_pieces = function(self)
+--end
+TestRoot.test_value_typecheck = function(self)
+    self.p:add(math.random(10), {1,1,1})
+    for _,v in ipairs({true, {}, function() return 3 end, 'string'}) do
+        assertError('value must be number, was: '..type(v),
+                    self.p.root, self.p, v)
+    end
+end
 
-pp:add(2, {8})
-tmp = pp:getStarts()
-assert(#tmp == 3, 'three pieces now')
-assert(tmp[1] == 0 and tmp[2] == 2 and tmp[3] == 3, 'confirm insert')
-assert(pp:evaluate(2.2) == 8, 'middling constant function; alias')
-assert(pp(3) == -0.5*3^3 + 3*3^2 + 2, 'third piece at play again')
 
-pp:clearBefore(2.2)
-tmp = pp:getStarts()
-assert(#tmp == 2, 'trim any pieces before 2')
-assert(tmp[1] == 2.2 and tmp[2] == 3)
-assert(pp(2.99) == 8)
-assert(pp(3) == -0.5*3^3 + 3*3^2 + 2, '"third" piece at play again')
-assert(pp(2.199) == nil, 'pre-trim is now undefined')
-
--- add piece before previous start
-pp:add(0.5, {1, 0})
-assert(pp(1) == 1)
-assert(pp(0.4) == nil)
-assert(pp(2.5) == 8)
-assert(pp(3) == -0.5*3^3 + 3*3^2 + 2, '"third" piece at play again')
-
---TODO: test equality
-
-print('==== TEST PIECEWISE POLYNOMIAL PASSED ====')
+--print('==== TEST PIECEWISE POLYNOMIAL PASSED ====')
+luaunit:run()
 
